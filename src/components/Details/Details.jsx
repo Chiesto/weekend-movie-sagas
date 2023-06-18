0b8c4f01d3f8ae769cd18524d489d4cd70f8c5a5
Chris Clark <xchrisck@gmail.com>
2023-06-18 15:19:36 -0500

    added a button and some conditionals to the details page to allow for the user to edit the name and description.

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,31 +1,60 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 
 
 function Details(){
+    const [isEdit, setIsEdit] = useState(false);
     const dispatch = useDispatch();
     const history = useHistory();
     const movieDetails = useSelector(store=>store.movieDetails);
 
+    const handleSaveClick = ()=>{
+        //initiate the PUT function to edit our data on the DB
+        dispatch('EDIT_MOVIE', {
+            title,
+            description
+        })
+        //get the updated data from the DB
+        dispatch('FETCH_DETAILS')
+        setIsEdit(false);
+    }
+
+
     useEffect(()=>{
-        dispatch({type: 'FETCH_DETAILS'})
+        dispatch({type: 'FETCH_DETAILS'});
     }, []);
-    console.log('movie details =>', movieDetails);
     const recentMovie = movieDetails[movieDetails.length-1];
     console.log('RECENT MOVIE =>', recentMovie);
+    console.log('isEdit =>', isEdit);
     return(
         <div>
             <div>
                 {recentMovie?(
-                    <>
-                        <h1>{recentMovie.title[0]}</h1>
-                        <img src={recentMovie.poster[0]}/>
-                    
+                    <> 
+                    {isEdit?(
+                        <>
+                            <input placeholder={recentMovie.title[0]}/>
+                            <br/>
+                            <img src={recentMovie.poster[0]}/>
                         
-                        <p>Genres: {recentMovie.genres.map(genre=><li key={genre}>{genre}</li>)}</p>
-                    
-                        <p>{movieDetails[movieDetails.length-1].description[0]}</p>
+                            
+                            <p>Genres: {recentMovie.genres.map(genre=><li key={genre}>{genre}</li>)}</p>
+                        
+                            <input type='text' placeholder={recentMovie.description[0]}/>
+                        </>
+                    ):(
+                        <>
+                            <h1>{recentMovie.title[0]}</h1>
+                            <img src={recentMovie.poster[0]}/>
+                        
+                            
+                            <p>Genres: {recentMovie.genres.map(genre=><li key={genre}>{genre}</li>)}</p>
+                        
+                            <p>{movieDetails[movieDetails.length-1].description[0]}</p>
+                        </>
+                    )}
+                        
                     </>
                 ):(
                     <p></p>
@@ -35,6 +64,12 @@ function Details(){
             </div>
 
             <button onClick={()=>history.push('/')}>Go Back</button>
+            {isEdit?(
+                <button onClick={()=>setIsEdit(false)}>Save</button>
+            ):(
+                <button onClick={()=>setIsEdit(true)}>Edit</button>
+            )}
+            
         </div>
     )
 }
